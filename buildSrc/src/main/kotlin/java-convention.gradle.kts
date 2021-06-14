ebbff76635c8717e9f657593ffd4c1e66ca3a4f7
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-14 17:08:21 +0300

    Fix the package name.

--- buildSrc/src/main/kotlin/java-convention.gradle.kts
+++ buildSrc/src/main/kotlin/java-convention.gradle.kts
@@ -80,7 +80,7 @@ tasks.compileJava {
     )
 
     options.errorprone {
-        option("NullAway:AnnotatedPackages", "io.spine.message-delivery")
+        option("NullAway:AnnotatedPackages", "io.spine.message.delivery")
         disableWarningsInGeneratedCode.set(true)
     }
 }
