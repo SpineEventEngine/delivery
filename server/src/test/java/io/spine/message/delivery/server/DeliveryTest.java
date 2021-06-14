98d2a8f4892591b191ae4248cbd1bb32f2194165
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-14 22:30:57 +0300

    Define abstract `DeliveryTest`.

---
+++ server/src/test/java/io/spine/message/delivery/server/DeliveryTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.server;
+
+import io.spine.server.BoundedContextBuilder;
+import io.spine.testing.server.blackbox.ContextAwareTest;
+
+/**
+ * An abstract base for {@link DeliveryContext} tests.
+ */
+@SuppressWarnings("AbstractClassWithoutAbstractMethods") // that's intentional.
+abstract class DeliveryTest extends ContextAwareTest {
+
+    @Override
+    protected BoundedContextBuilder contextBuilder() {
+        return DeliveryContext.newBuilder()
+                .contextBuilder();
+    }
+}
