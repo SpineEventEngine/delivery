3ba4b1d0041ad270ae0546bd01b8324a33adce7c
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-14 17:07:35 +0300

    Add Checkstyle configuration.

--- buildSrc/src/main/kotlin/code-quality.gradle.kts
+++ buildSrc/src/main/kotlin/code-quality.gradle.kts
@@ -1,8 +1,10 @@
+import io.spine.internal.dependency.CheckStyle
 import io.spine.internal.dependency.Pmd
 
 plugins {
     java
     pmd
+    checkstyle
 }
 
 pmd {
@@ -16,5 +18,17 @@ pmd {
     ruleSetFiles = files("${rootDir}/buildSrc/src/main/resources/pmd.xml")
 
     reportsDir = file("build/reports/pmd")
+    sourceSets = listOf(project.sourceSets.named("main").get())
+}
+
+checkstyle {
+    toolVersion = CheckStyle.version
+
+    // The build is going to fail in case of violations.
+    isIgnoreFailures = false
+
+    configFile = file("${rootDir}/buildSrc/src/main/resources/checkstyle.xml")
+
+    reportsDir = file("build/reports/checkstyle")
     sourceSets = listOf(project.sourceSets.named("main").get())
 }
