a6533073ac1337923d54c52799a1e55d7e27f874
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-14 21:41:08 +0300

    Add InboxStorage projection.

---
+++ server/src/main/java/io/spine/message/delivery/server/InboxWriter.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.server;
+
+import io.spine.base.Identifier;
+import io.spine.core.CommandContext;
+import io.spine.logging.Logging;
+import io.spine.message.delivery.server.command.WriteMessage;
+import io.spine.message.delivery.server.event.MessageWritten;
+import io.spine.server.command.AbstractCommandHandler;
+import io.spine.server.command.Assign;
+
+/**
+ * Handles {@link WriteMessage} commands to store new {@link io.spine.server.delivery.Inbox Inbox}
+ * messages.
+ */
+final class InboxWriter extends AbstractCommandHandler implements Logging {
+
+    @Assign
+    MessageWritten handle(WriteMessage c, CommandContext context) {
+        _debug().log("Writing a new message for Inbox `%s` on `%s` request.",
+                     Identifier.toString(c.getInbox()), context.actor());
+        return MessageWritten
+                .newBuilder()
+                .setInbox(c.getInbox())
+                .setMessage(c.getMessage())
+                .vBuild();
+    }
+}
