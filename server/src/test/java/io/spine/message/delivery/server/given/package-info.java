6ba5e729a002e55282912ec34c00afb0f13646ba
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-14 22:34:50 +0300

    Define test inbox messages helper.

---
+++ server/src/test/java/io/spine/message/delivery/server/given/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Test environment classes for testing {@code io.spine.message.delivery.server} package.
+ */
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package io.spine.message.delivery.server.given;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+
+import javax.annotation.ParametersAreNonnullByDefault;
