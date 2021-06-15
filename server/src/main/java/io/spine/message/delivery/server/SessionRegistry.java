684c3ad5aee982919aced1265ed84dd7b7457233
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-15 10:18:22 +0300

    Check in `SessionRegistry` stub.

---
+++ server/src/main/java/io/spine/message/delivery/server/SessionRegistry.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.server;
+
+import io.spine.message.delivery.server.command.ShardSessionRegistry;
+import io.spine.server.aggregate.Aggregate;
+import io.spine.server.delivery.ShardIndex;
+
+final class SessionRegistry
+        extends Aggregate<ShardIndex, ShardSessionRegistry, ShardSessionRegistry.Builder> {
+
+
+}
