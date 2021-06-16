43bea18475265426bddaac4cbfb0ac245c43a858
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-16 22:20:46 +0300

    Create the `ShardedInboxStorage` projection stub.

---
+++ server/src/main/java/io/spine/message/delivery/server/ShardedInboxStorageState.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.server;
+
+import com.google.common.collect.Ordering;
+import io.spine.core.EventContext;
+import io.spine.core.Subscribe;
+import io.spine.message.delivery.server.event.MessageWritten;
+import io.spine.server.delivery.InboxMessageComparator;
+import io.spine.server.projection.Projection;
+
+//TODO:2021-06-16:yuri-sergiichuk: add docs.
+final class ShardedInboxStorageState
+        extends Projection<ShardedStorageId, ShardedInboxStorage, ShardedInboxStorage.Builder> {
+
+    //TODO:2021-06-16:yuri-sergiichuk: add repo and routing.
+
+    //TODO:2021-06-16:yuri-sergiichuk: add tests.
+    @Subscribe
+    void on(MessageWritten e, EventContext context) {
+        var messages = builder()
+                .addMessage(e.getMessage())
+                .getMessageList();
+        var sortedMessages =
+                Ordering.from(InboxMessageComparator.chronologically)
+                        .sortedCopy(messages);
+        builder().clearMessage()
+                 .addAllMessage(sortedMessages);
+    }
+}
