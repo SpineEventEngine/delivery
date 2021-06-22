47935694939a39a502862878bd086cf40bf0222a
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-22 21:31:19 +0300

    Add docs.

--- client/src/main/java/io/spine/message/delivery/client/SessionRegistryClient.java
+++ client/src/main/java/io/spine/message/delivery/client/SessionRegistryClient.java
@@ -9,24 +9,37 @@ package io.spine.message.delivery.client;
 import io.spine.message.delivery.event.ShardPickedUp;
 import io.spine.message.delivery.event.ShardReleased;
 import io.spine.server.NodeId;
-import io.spine.server.ServerEnvironment;
 import io.spine.server.delivery.ShardIndex;
 
 import java.util.Optional;
 
+/**
+ * A client library for working with the delivery session registry.
+ */
 interface SessionRegistryClient {
 
+    /**
+     * Tries to pick up a shard for working with it.
+     *
+     * @param shard
+     *         the shard to pick up
+     * @param worker
+     *         the node which would like to work with the shard
+     * @return the shard picked up acknowledgement event if the shard was picked up, empty otherwise
+     */
     Optional<ShardPickedUp> pickUpShard(ShardIndex shard, NodeId worker);
 
-    default Optional<ShardPickedUp> pickUpShard(ShardIndex shard) {
-        var worker = ServerEnvironment.instance().nodeId();
-        return pickUpShard(shard, worker);
-    }
-
+    /**
+     * Tries to release the shard.
+     *
+     * <p>The same node which picked up the shard must also release it. It is prohibited to release
+     * shards which are picked up by other workers.
+     *
+     * @param shard
+     *         the shard to be released
+     * @param worker
+     *         the node which would like to release the shard
+     * @return the shard released acknowledgement event if the shard was released, empty otherwise
+     */
     Optional<ShardReleased> releaseShard(ShardIndex shard, NodeId worker);
-
-    default Optional<ShardReleased> releaseShard(ShardIndex shard) {
-        var worker = ServerEnvironment.instance().nodeId();
-        return releaseShard(shard, worker);
-    }
 }
