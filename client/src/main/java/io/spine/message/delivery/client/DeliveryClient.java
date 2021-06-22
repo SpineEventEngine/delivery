4ebdb0810b3ee24062b3dde9588df4ea3ef13ea8
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-22 22:15:33 +0300

    Implement `SessionRegistryClient`.

--- client/src/main/java/io/spine/message/delivery/client/DeliveryClient.java
+++ client/src/main/java/io/spine/message/delivery/client/DeliveryClient.java
@@ -8,12 +8,24 @@ package io.spine.message.delivery.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.spine.base.CommandMessage;
+import io.spine.base.EventMessage;
 import io.spine.client.Client;
+import io.spine.logging.Logging;
+import io.spine.message.delivery.command.PickUpShard;
+import io.spine.message.delivery.command.ReleaseShard;
+import io.spine.message.delivery.event.ShardPickedUp;
+import io.spine.message.delivery.event.ShardReleased;
+import io.spine.server.NodeId;
+import io.spine.server.delivery.ShardIndex;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import static io.spine.util.Preconditions2.checkNotEmptyOrBlank;
 import static io.spine.util.Preconditions2.checkPositive;
 
-final class DeliveryClient {
+final class DeliveryClient implements SessionRegistryClient, Logging {
 
     private final Client client;
 
@@ -43,5 +55,54 @@ final class DeliveryClient {
                 .usePlaintext()
                 .build();
         return new DeliveryClient(channel);
+    }
+
+    @Override
+    public Optional<ShardPickedUp> pickUpShard(ShardIndex shard, NodeId worker) {
+        var pickUpShard = PickUpShard.newBuilder()
+                .setShard(shard)
+                .setWorker(worker)
+                .vBuild();
+        var result = postCommand(pickUpShard, ShardPickedUp.class);
+        return result;
+    }
+
+    @Override
+    public Optional<ShardReleased> releaseShard(ShardIndex shard, NodeId worker) {
+        var releaseShard = ReleaseShard.newBuilder()
+                .setShard(shard)
+                .setWorker(worker)
+                .vBuild();
+        var result = postCommand(releaseShard, ShardReleased.class);
+        return result;
+    }
+
+    private <C extends CommandMessage, E extends EventMessage> Optional<E>
+    postCommand(C command, Class<E> event) {
+        _trace().log("Posting command `%s` and waiting for a response event `%s`.",
+                     command.getClass(), event);
+        var future = new CompletableFuture<Optional<E>>();
+        var subscriptions =
+                client.asGuest()
+                      .command(command)
+                      .observe(event, e -> {
+                          _trace().log(
+                                  "Received an event `%s` in response for a command `%s`.",
+                                  event, command.getClass()
+                          );
+                          future.complete(Optional.of(e));
+                      })
+                      .onServerError((msg, error) -> {
+                          _trace().log(
+                                  "Server was not able to handle command `%s`: %s",
+                                  command.getClass(), error
+                          );
+                          future.complete(Optional.empty());
+                      })
+                      .onStreamingError(future::completeExceptionally)
+                      .post();
+        var result = future.join();
+        subscriptions.forEach(client.subscriptions()::cancel);
+        return result;
     }
 }
