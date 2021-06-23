aba5d0c732bbd57f7626b43617949b4adba2a15f
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-23 22:33:01 +0300

    Add shadow jar and jib configs.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -4,12 +4,47 @@
  * Use is subject to license terms.
  */
 
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import io.spine.internal.dependency.Grpc
 import io.spine.internal.dependency.Spine
+
+plugins {
+    id("com.google.cloud.tools.jib")
+    id("com.github.johnrengelman.shadow")
+    id("application")
+}
+
+/** The GCP project ID used for deployment of the application. **/
+//val gcpProject: String by project
+val gcpProject = ""
 
 dependencies {
     runtimeOnly(Grpc.nettyShaded)
     implementation(project(":model"))
     implementation(Spine.server)
     testImplementation(Spine.Test.server)
+}
+
+val shadowJar: ShadowJar by tasks
+shadowJar.apply {
+    mergeServiceFiles()
+    mergeServiceFiles("desc.ref")
+    manifest {
+        attributes["Multi-Release"] = "true" // https://github.com/johnrengelman/shadow/issues/449
+    }
+}
+
+application {
+    mainClass.set("io.spine.message.delivery.server.App")
+}
+
+jib {
+    to {
+        image = "gcr.io/${gcpProject}/message-delivery-server"
+        tags = setOf("latest")
+    }
+    container {
+        mainClass = application.mainClass.get()
+        ports = listOf("8080", "8484")
+    }
 }
