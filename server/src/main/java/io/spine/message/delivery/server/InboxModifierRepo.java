7f21f4f007d6f475b78bdd2cdb339f25780264c6
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-25 17:31:06 +0300

    Cover new commands with tests.

--- server/src/main/java/io/spine/message/delivery/server/InboxModifierRepo.java
+++ server/src/main/java/io/spine/message/delivery/server/InboxModifierRepo.java
@@ -6,14 +6,18 @@
 
 package io.spine.message.delivery.server;
 
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
 import io.spine.message.delivery.InboxModifier;
 import io.spine.message.delivery.InboxModifierId;
 import io.spine.message.delivery.command.RemoveMessage;
 import io.spine.message.delivery.command.RemoveMessages;
 import io.spine.message.delivery.command.WriteMessage;
 import io.spine.message.delivery.command.WriteMessages;
+import io.spine.message.delivery.event.MessagesRemoved;
+import io.spine.message.delivery.event.MessagesWritten;
 import io.spine.server.procman.ProcessManagerRepository;
 import io.spine.server.route.CommandRouting;
+import io.spine.server.route.EventRouting;
 
 /**
  * Routes all {@link WriteMessage} commands to the {@link InboxModifierProcess}.
@@ -34,5 +38,13 @@ final class InboxModifierRepo
                .route(WriteMessages.class, (message, context) -> writer)
                .route(RemoveMessage.class, (message, context) -> writer)
                .route(RemoveMessages.class, (message, context) -> writer);
+    }
+
+    @Override
+    @OverridingMethodsMustInvokeSuper
+    protected void setupEventRouting(EventRouting<InboxModifierId> routing) {
+        super.setupEventRouting(routing);
+        routing.unicast(MessagesRemoved.class, e -> writer)
+               .unicast(MessagesWritten.class, e -> writer);
     }
 }
--- server/src/test/java/io/spine/message/delivery/server/InboxModifierTest.java
+++ server/src/test/java/io/spine/message/delivery/server/InboxModifierTest.java
@@ -7,11 +7,18 @@
 package io.spine.message.delivery.server;
 
 import io.spine.base.Identifier;
+import io.spine.message.delivery.command.RemoveMessage;
+import io.spine.message.delivery.command.RemoveMessages;
 import io.spine.message.delivery.command.WriteMessage;
+import io.spine.message.delivery.command.WriteMessages;
+import io.spine.message.delivery.event.MessageRemoved;
 import io.spine.message.delivery.event.MessageWritten;
+import io.spine.message.delivery.event.MessagesRemoved;
+import io.spine.message.delivery.event.MessagesWritten;
 import io.spine.message.delivery.server.given.TestInboxMessages;
 import io.spine.server.delivery.InboxMessage;
 import io.spine.test.message.delivery.server.Something;
+import io.spine.testing.server.EventSubject;
 import io.spine.type.TypeUrl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,6 +50,131 @@ final class InboxModifierTest extends DeliveryTest {
                     .setMessage(message)
                     .vBuild();
             context().assertEvent(messageWritten);
+        }
+    }
+
+    @Nested
+    @DisplayName("handle a `WriteMessages` command")
+    final class HandleWriteMessages {
+
+        private final InboxMessage firstMessage = TestInboxMessages
+                .toDeliver(Identifier.newUuid(), TypeUrl.of(Something.class));
+        private final InboxMessage secondMessage = TestInboxMessages
+                .toDeliver(Identifier.newUuid(), TypeUrl.of(Something.class));
+
+        @BeforeEach
+        void writeMessages() {
+            var writeMessages = WriteMessages.newBuilder()
+                    .addMessage(firstMessage)
+                    .addMessage(secondMessage)
+                    .setShard(firstMessage.shardIndex())
+                    .vBuild();
+            context().receivesCommand(writeMessages);
+        }
+
+        @Test
+        @DisplayName("emitting `MessagesWritten` event")
+        void event() {
+            var messagesWritten = MessagesWritten.newBuilder()
+                    .addMessage(firstMessage)
+                    .addMessage(secondMessage)
+                    .setShard(firstMessage.shardIndex())
+                    .vBuild();
+            context().assertEvent(messagesWritten);
+        }
+
+        @Test
+        @DisplayName("emitting `MessageWritten` events for each of the removed messages")
+        void removeEachMessage() {
+            var firstWritten = MessageWritten.newBuilder()
+                    .setMessage(firstMessage)
+                    .vBuild();
+            var secondWritten = MessageWritten.newBuilder()
+                    .setMessage(secondMessage)
+                    .vBuild();
+            EventSubject writtenMessages = context()
+                    .assertEvents()
+                    .withType(MessageWritten.class);
+            writtenMessages.hasSize(2);
+            writtenMessages.message(0)
+                           .isEqualTo(firstWritten);
+            writtenMessages.message(1)
+                           .isEqualTo(secondWritten);
+        }
+    }
+
+    @Nested
+    @DisplayName("handle a `RemoveMessage` command")
+    final class HandleRemoveMessage {
+
+        private final InboxMessage message = TestInboxMessages
+                .toDeliver(Identifier.newUuid(), TypeUrl.of(Something.class));
+
+        @BeforeEach
+        void removeMessage() {
+            var removeMessage = RemoveMessage.newBuilder()
+                    .setMessage(message)
+                    .vBuild();
+            context().receivesCommand(removeMessage);
+        }
+
+        @Test
+        @DisplayName("emitting `MessageRemoved` event")
+        void event() {
+            var messageRemoved = MessageRemoved.newBuilder()
+                    .setMessage(message)
+                    .vBuild();
+            context().assertEvent(messageRemoved);
+        }
+    }
+
+    @Nested
+    @DisplayName("handle a `RemoveMessages` command")
+    final class HandleRemoveMessages {
+
+        private final InboxMessage firstMessage = TestInboxMessages
+                .toDeliver(Identifier.newUuid(), TypeUrl.of(Something.class));
+        private final InboxMessage secondMessage = TestInboxMessages
+                .toDeliver(Identifier.newUuid(), TypeUrl.of(Something.class));
+
+        @BeforeEach
+        void removeMessages() {
+            var removeMessages = RemoveMessages.newBuilder()
+                    .addMessage(firstMessage)
+                    .addMessage(secondMessage)
+                    .setShard(firstMessage.shardIndex())
+                    .vBuild();
+            context().receivesCommand(removeMessages);
+        }
+
+        @Test
+        @DisplayName("emitting `MessagesRemoved` event")
+        void event() {
+            var messagesRemoved = MessagesRemoved.newBuilder()
+                    .addMessage(firstMessage)
+                    .addMessage(secondMessage)
+                    .setShard(firstMessage.shardIndex())
+                    .vBuild();
+            context().assertEvent(messagesRemoved);
+        }
+
+        @Test
+        @DisplayName("emitting `MessageRemove` events for each of the removed messages")
+        void removeEachMessage() {
+            var firstRemoved = MessageRemoved.newBuilder()
+                    .setMessage(firstMessage)
+                    .vBuild();
+            var secondRemoved = MessageRemoved.newBuilder()
+                    .setMessage(secondMessage)
+                    .vBuild();
+            EventSubject removedMessages = context()
+                    .assertEvents()
+                    .withType(MessageRemoved.class);
+            removedMessages.hasSize(2);
+            removedMessages.message(0)
+                           .isEqualTo(firstRemoved);
+            removedMessages.message(1)
+                           .isEqualTo(secondRemoved);
         }
     }
 }
