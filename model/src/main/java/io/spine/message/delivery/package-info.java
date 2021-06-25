12802f52a2e7ad86e0b4b0f2ab07970d8cf225f8
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-25 17:13:27 +0300

    Add `WithInboxMessage` mixin.

---
+++ model/src/main/java/io/spine/message/delivery/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+/**
+ * Root package of the Message Delivery application.
+ */
+package io.spine.message.delivery;
