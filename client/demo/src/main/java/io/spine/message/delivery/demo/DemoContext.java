b724fdc2fc550211339cdaedee826ea7f0aac052
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-30 23:36:25 +0300

    Add a stub DemoContext.

---
+++ client/demo/src/main/java/io/spine/message/delivery/demo/DemoContext.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.demo;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.spine.server.BoundedContext;
+import io.spine.server.BoundedContextBuilder;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * Represents the context of this demo application.
+ */
+final class DemoContext {
+
+    private static final Random random = new SecureRandom();
+    static final String contextName = "Demo";
+
+    /**
+     * Prevents instantiation.
+     */
+    private DemoContext() {
+    }
+
+    /**
+     * Returns a new instance of this context.
+     */
+    static BoundedContext newInstance() {
+        return builder().build();
+    }
+
+    /**
+     * Returns a fully-initialized context builder.
+     */
+    @VisibleForTesting
+    static BoundedContextBuilder builder() {
+        return BoundedContext
+                .singleTenant(contextName)
+                .add(new GreeterRepo(random));
+    }
+}
