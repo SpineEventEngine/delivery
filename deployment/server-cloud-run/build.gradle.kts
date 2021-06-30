cd70d4cb61df21a4ab4b207c5da39f8271a42f97
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-06-30 11:09:30 +0300

    Extract Server deployment into a separate module.

---
+++ deployment/server-cloud-run/build.gradle.kts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import io.spine.internal.dependency.Flogger
+import io.spine.internal.dependency.Grpc
+import io.spine.internal.dependency.Log4j2
+
+plugins {
+    id("com.google.cloud.tools.jib")
+    id("com.github.johnrengelman.shadow")
+}
+
+/** The GCP project ID used for deployment of the application. **/
+//val gcpProject: String by project
+val gcpProject = "bko-dev-firestore"
+
+dependencies {
+    runtimeOnly(Grpc.nettyShaded)
+    runtimeOnly(Log4j2.slf4jBridge)
+    runtimeOnly(Log4j2.core)
+    runtimeOnly(Flogger.Runtime.log4J2)
+    implementation(project(":server"))
+}
+
+val appClassName = "io.spine.message.delivery.server.App"
+
+tasks.withType<ShadowJar> {
+    mergeServiceFiles()
+    mergeServiceFiles("desc.ref")
+    manifest {
+        attributes["Multi-Release"] = "true" // https://github.com/johnrengelman/shadow/issues/449
+        attributes["Main-Class"] = appClassName
+    }
+}
+
+jib {
+    to {
+        image = "gcr.io/${gcpProject}/message-delivery-server"
+        tags = setOf("latest")
+    }
+    container {
+        mainClass = appClassName
+        ports = listOf("8080", "8484")
+    }
+}
