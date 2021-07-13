2c270a943a4b2a0327341d822bf14d90bf987b1c
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-07-13 22:40:18 +0300

    Create stub tests holder.

---
+++ server/src/test/java/io/spine/message/delivery/server/grpc/SessionRegistryServiceTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2000-2021 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.server.grpc;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("`SessionRegistryService` should")
+final class SessionRegistryServiceTest {
+
+    //TODO:2021-07-13:yuri-sergiichuk: add tests.
+}
