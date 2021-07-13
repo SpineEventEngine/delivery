b062612039b21096b69d488243b20141cdb3c52e
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-07-13 17:32:09 +0300

    Define `ShardSessionRegistryService`.

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -4,8 +4,13 @@
  * Use is subject to license terms.
  */
 
+import io.spine.internal.dependency.Grpc
 import io.spine.internal.dependency.Spine
 
 dependencies {
+    Grpc.apply {
+        api(stub)
+        api(protobuf)
+    }
     implementation(Spine.server)
 }
