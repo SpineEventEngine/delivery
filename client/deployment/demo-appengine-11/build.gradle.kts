cda5e0c5366024b9b31cdd8b86ced9185fa4dbc2
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-07-19 16:27:22 +0300

    By default start the app with the debug port open.

--- client/deployment/demo-appengine-11/build.gradle.kts
+++ client/deployment/demo-appengine-11/build.gradle.kts
@@ -42,6 +42,12 @@ tasks.withType<ShadowJar> {
     destinationDirectory.set(file(uberJarFolder))
 }
 
+application {
+    applicationDefaultJvmArgs = listOf(
+        "-Xdebug", "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+    )
+}
+
 appengine {
     deploy {
         projectId = gcpProject
