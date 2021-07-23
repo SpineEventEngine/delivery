72c63860b3b067462488c03ea11c3d25fab1103d
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-07-23 15:03:16 +0300

    Add testcontainers dependency

--- redis-record-storage/build.gradle.kts
+++ redis-record-storage/build.gradle.kts
@@ -1,6 +1,9 @@
 import io.spine.internal.dependency.Spine
+import io.spine.internal.dependency.Testcontainers
 
 dependencies {
     api(Spine.server)
     implementation("org.redisson:redisson:3.16.0")
+    testImplementation(Testcontainers.lib)
+    testImplementation(Testcontainers.junitJupiter)
 }
