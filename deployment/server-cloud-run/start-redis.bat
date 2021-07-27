e2a820b79c5f68fad2a12fc71cd6ec3e495a0fbb
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-07-27 09:16:42 +0300

    Add comments instead of just having empty space.

--- deployment/server-cloud-run/start-redis.bat
+++ deployment/server-cloud-run/start-redis.bat
@@ -1,2 +1,5 @@
+@rem ##########################################################################################
+@rem # Starts Redis docker container with a default Redis 6379 port exposed to the localhost. #
+@rem ##########################################################################################
 
 docker run --rm --name "message-delivery-server-redis" -p 6379:6379 -d "redis:6-alpine"
