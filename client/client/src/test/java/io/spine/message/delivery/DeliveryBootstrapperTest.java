d7155c43840a05ef79d4bf467ec6c8e7ce71e527
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-08-05 14:11:18 +0300

    Move reusable stub to a shared test module.

--- client/client/src/test/java/io/spine/message/delivery/DeliveryBootstrapperTest.java
+++ client/client/src/test/java/io/spine/message/delivery/DeliveryBootstrapperTest.java
@@ -7,15 +7,9 @@
 package io.spine.message.delivery;
 
 import com.google.common.testing.NullPointerTester;
-import io.grpc.CallOptions;
-import io.grpc.ClientCall;
-import io.grpc.ManagedChannel;
-import io.grpc.MethodDescriptor;
 import io.spine.server.delivery.DeliveryBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.TimeUnit;
 
 import static com.google.common.truth.Truth8.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -44,52 +38,5 @@ final class DeliveryBootstrapperTest {
                 .isPresent();
         assertThat(builder.workRegistry())
                 .isPresent();
-    }
-
-    /**
-     * A test-only stub channel which does nothing.
-     */
-    private static class NoOpChannel extends ManagedChannel {
-
-        @Override
-        public ManagedChannel shutdown() {
-            throw notSupported();
-        }
-
-        @Override
-        public boolean isShutdown() {
-            return false;
-        }
-
-        @Override
-        public boolean isTerminated() {
-            return false;
-        }
-
-        @Override
-        public ManagedChannel shutdownNow() {
-            throw notSupported();
-        }
-
-        @Override
-        public boolean awaitTermination(long timeout, TimeUnit unit) {
-            return false;
-        }
-
-        @Override
-        public <RequestT, ResponseT> ClientCall<RequestT, ResponseT> newCall(
-                MethodDescriptor<RequestT, ResponseT> methodDescriptor,
-                CallOptions callOptions) {
-            throw notSupported();
-        }
-
-        @Override
-        public String authority() {
-            throw notSupported();
-        }
-
-        private static UnsupportedOperationException notSupported() {
-            throw new UnsupportedOperationException("Not supported.");
-        }
     }
 }
