dc00c56ec09e84c964cd029ad86a97af63fcee77
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-10-01 10:30:44 +0300

    Expose shard session release API which provides more info about the session.

--- simple-server/src/main/java/io/spine/message/delivery/server/ExtendedShardRegistry.java
+++ simple-server/src/main/java/io/spine/message/delivery/server/ExtendedShardRegistry.java
@@ -6,7 +6,10 @@
 
 package io.spine.message.delivery.server;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Duration;
+import com.google.protobuf.Timestamp;
+import com.google.protobuf.util.Durations;
 import io.spine.server.NodeId;
 import io.spine.server.delivery.AbstractWorkRegistry;
 import io.spine.server.delivery.ShardIndex;
@@ -18,6 +21,8 @@ import java.util.Iterator;
 import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.protobuf.util.Timestamps.between;
+import static io.spine.base.Time.currentTime;
 
 /**
  * A {@link ShardRegistryStorage}-baked {@link io.spine.server.delivery.ShardedWorkRegistry
@@ -45,6 +50,36 @@ public final class ExtendedShardRegistry extends AbstractWorkRegistry {
     @Override
     public synchronized Iterable<ShardIndex> releaseExpiredSessions(Duration inactivityPeriod) {
         return super.releaseExpiredSessions(inactivityPeriod);
+    }
+
+    /**
+     * Clears up the recorded {@code NodeId}s from the session records if there was no activity
+     * for longer than passed {@code inactivityPeriod}.
+     *
+     * <p>It may be handy if an application node hangs or gets killed — so that it is not able
+     * to complete the session in a conventional way.
+     *
+     * @implNote A copy of the {@link #releaseExpiredSessions(Duration)} which returns the
+     *         whole {@code ShardSessionRecord} instead of the {@code ShardIndex}.
+     * @see #releaseExpiredSessions(Duration)
+     */
+    public synchronized ImmutableSet<ShardSessionRecord>
+    releaseInactiveSessions(Duration inactivityPeriod) {
+        checkNotNull(inactivityPeriod);
+        ImmutableSet.Builder<ShardSessionRecord> resultBuilder = ImmutableSet.builder();
+        allRecords().forEachRemaining(record -> {
+            if (record.hasPickedBy()) {
+                Timestamp whenPicked = record.getWhenLastPicked();
+                Duration elapsed = between(whenPicked, currentTime());
+
+                int comparison = Durations.compare(elapsed, inactivityPeriod);
+                if (comparison >= 0) {
+                    clearNode(record);
+                    resultBuilder.add(record);
+                }
+            }
+        });
+        return resultBuilder.build();
     }
 
     @Override
