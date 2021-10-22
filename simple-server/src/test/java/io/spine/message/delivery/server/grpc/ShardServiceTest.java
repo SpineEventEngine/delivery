41d26d9e75f56221cd3d8ce78f97ffaff1034dd1
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-10-22 13:58:26 +0300

    Add `ReleaseExpired` shards tests.

--- simple-server/src/test/java/io/spine/message/delivery/server/grpc/ShardServiceTest.java
+++ simple-server/src/test/java/io/spine/message/delivery/server/grpc/ShardServiceTest.java
@@ -6,12 +6,17 @@
 
 package io.spine.message.delivery.server.grpc;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.spine.message.delivery.command.PickUpShard;
+import io.spine.message.delivery.command.ReleaseExpiredSessions;
 import io.spine.message.delivery.command.ReleaseShard;
+import io.spine.message.delivery.event.ExpiredSession;
+import io.spine.message.delivery.event.ExpiredSessionsReleased;
 import io.spine.message.delivery.event.ShardPickedUp;
 import io.spine.message.delivery.server.WithApp;
+import io.spine.protobuf.Durations2;
 import io.spine.server.NodeId;
 import io.spine.server.ServerEnvironment;
 import io.spine.server.delivery.DeliveryStrategy;
@@ -19,6 +24,8 @@ import io.spine.server.delivery.ShardIndex;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
@@ -28,17 +35,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DisplayName("`ShardService` should")
 final class ShardServiceTest {
 
-    @Nested
-    @DisplayName("process `pickShard` request")
-    final class Pick extends WithApp {
+    private static final NodeId worker = ServerEnvironment.instance()
+            .nodeId();
+    private static final ShardIndex shard = DeliveryStrategy.newIndex(0, 1);
+    private static final PickUpShard pickUpShard = PickUpShard.newBuilder()
+            .setShard(shard)
+            .setWorker(worker)
+            .vBuild();
 
-        private final NodeId worker = ServerEnvironment.instance()
-                .nodeId();
-        private final ShardIndex shard = DeliveryStrategy.newIndex(0, 1);
-        private final PickUpShard pickUpShard = PickUpShard.newBuilder()
-                .setShard(shard)
-                .setWorker(worker)
-                .vBuild();
+    @Nested
+    @DisplayName("process `PickUpShard` request")
+    final class Pick extends WithApp {
 
         @Test
         @DisplayName("picking up available shard")
@@ -72,16 +79,9 @@ final class ShardServiceTest {
     }
 
     @Nested
-    @DisplayName("process `releaseShard` request")
+    @DisplayName("process `ReleaseShard` request")
     final class Release extends WithApp {
 
-        private final NodeId worker = ServerEnvironment.instance()
-                .nodeId();
-        private final ShardIndex shard = DeliveryStrategy.newIndex(0, 1);
-        private final PickUpShard pickUpShard = PickUpShard.newBuilder()
-                .setShard(shard)
-                .setWorker(worker)
-                .vBuild();
         private final ReleaseShard releaseShard = ReleaseShard.newBuilder()
                 .setShard(shard)
                 .setWorker(worker)
@@ -102,6 +102,40 @@ final class ShardServiceTest {
                 shardService.releaseSession(releaseShard);
                 shardService.pickShard(pickUpShard);
             });
+        }
+    }
+
+    @Nested
+    @DisplayName("process `ReleaseExpiredSessions` request")
+    final class ReleaseExpired extends WithApp {
+
+        private final ReleaseExpiredSessions request = ReleaseExpiredSessions.newBuilder()
+                .setInactivityPeriod(Durations2.seconds(2))
+                .vBuild();
+
+        @Test
+        @DisplayName("doing nothing when no shards are picked up")
+        void doNothing() {
+            var response = syncShardService().releaseSessions(request);
+            assertThat(response)
+                    .isEqualToDefaultInstance();
+        }
+
+        @Test
+        @DisplayName("release shards picked up earlier than supplied inactivity period")
+        void releaseExpired() {
+            var shardService = syncShardService();
+            var shardPickedUp = shardService.pickShard(pickUpShard);
+            Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+            var expected = ExpiredSessionsReleased.newBuilder()
+                    .addShard(ExpiredSession.newBuilder()
+                                      .setShard(shardPickedUp.getShard())
+                                      .setPickedBy(shardPickedUp.getPickedBy()))
+                    .buildPartial();
+            var response = shardService.releaseSessions(request);
+            assertThat(response)
+                    .comparingExpectedFieldsOnly()
+                    .isEqualTo(expected);
         }
     }
 }
