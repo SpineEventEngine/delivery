319efe44eb264c08f4ae876e3d2d22c79a9b075f
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-01-10 13:02:14 +0100

    Move strategy field to the abstract class.

--- client/client/src/main/java/io/spine/message/delivery/DeliveryBootstrapper.java
+++ client/client/src/main/java/io/spine/message/delivery/DeliveryBootstrapper.java
@@ -8,15 +8,13 @@ package io.spine.message.delivery;
 
 import io.grpc.ManagedChannel;
 import io.spine.message.delivery.client.DeliveryClient;
-import io.spine.message.delivery.client.RequestExecutionStrategy;
 import io.spine.message.delivery.client.InboxClient;
 import io.spine.message.delivery.client.Propagate;
+import io.spine.message.delivery.client.RequestExecutionStrategy;
 import io.spine.message.delivery.client.SessionRegistryClient;
 import io.spine.server.delivery.Delivery;
 
 import java.util.function.Supplier;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Provides fluent API for building a {@link Delivery} backed by the Message Delivery Server and
@@ -25,8 +23,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * <p> By default uses the {@link Propagate} strategy.
  */
 public final class DeliveryBootstrapper extends AbstractDeliveryBootstrapper<DeliveryBootstrapper> {
-
-    private RequestExecutionStrategy strategy = new Propagate();
 
     /**
      * Prevents direct instantiation.
@@ -42,15 +38,6 @@ public final class DeliveryBootstrapper extends AbstractDeliveryBootstrapper<Del
         return new DeliveryBootstrapper();
     }
 
-    /**
-     * Configures delivery to be using the given {@code strategy} to handle possible failures
-     * during interaction with server.
-     */
-    public DeliveryBootstrapper withErrorStrategy(RequestExecutionStrategy strategy) {
-        this.strategy = checkNotNull(strategy);
-        return self();
-    }
-
     @Override
     protected DeliveryBootstrapper self() {
         return this;
@@ -58,11 +45,11 @@ public final class DeliveryBootstrapper extends AbstractDeliveryBootstrapper<Del
 
     @Override
     protected InboxClient newInboxClient(Supplier<ManagedChannel> channel) {
-        return DeliveryClient.create(channel.get(), strategy);
+        return DeliveryClient.create(channel.get(), executionStrategy());
     }
 
     @Override
     protected SessionRegistryClient newSessionRegistryClient(Supplier<ManagedChannel> channel) {
-        return DeliveryClient.create(channel.get(), strategy);
+        return DeliveryClient.create(channel.get(), executionStrategy());
     }
 }
