25f7448955ab61da3feed2eea0268d0f6f48a2a1
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-01-16 12:31:58 +0100

    Move inner classes to a package level.

---
+++ client/base/src/main/java/io/spine/message/delivery/client/strategy/FailureReport.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.client.strategy;
+
+import com.google.common.collect.ImmutableList;
+import io.spine.message.delivery.client.ExecutionFailedException;
+
+/**
+ * Summary of the {@code VoidRequest} failure.
+ */
+class FailureReport {
+
+    private final Runnable retry;
+
+    private final RuntimeException lastException;
+
+    private final ImmutableList<RuntimeException> previousExceptions;
+
+    FailureReport(Runnable retry,
+                  ImmutableList<RuntimeException> previousExceptions,
+                  RuntimeException lastException) {
+        this.retry = retry;
+        this.previousExceptions = previousExceptions;
+        this.lastException = lastException;
+    }
+
+    /**
+     * Returns a predefined {@code Action} that retries failed {@code VoidRequest}.
+     */
+    public Action retry() {
+        return retry::run;
+    }
+
+    /**
+     * Returns a predefined {@code Action} that propagates current and previously occurred
+     * exceptions as an {@code ExecutionFailedException}.
+     */
+    public Action propagate() {
+        ImmutableList<RuntimeException> exceptions = ImmutableList.<RuntimeException>builder()
+                .addAll(previousExceptions)
+                .add(lastException)
+                .build();
+        throw new ExecutionFailedException(exceptions);
+    }
+
+    /**
+     * Returns last occurred exception.
+     */
+    public Exception lastException() {
+        return lastException;
+    }
+
+    /**
+     * Returns a list of all previously occurred exceptions.
+     */
+    public ImmutableList<RuntimeException> previousExceptions() {
+        return previousExceptions;
+    }
+}
---
+++ client/base/src/main/java/io/spine/message/delivery/client/strategy/FailureReportForNonVoidRequest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.client.strategy;
+
+import com.google.common.collect.ImmutableList;
+import io.spine.message.delivery.client.ExecutionFailedException;
+
+import java.util.function.Supplier;
+
+/**
+ * Summary of the {@code RequestWithResult} failure.
+ *
+ * @param <R>
+ *         type of result that have to be returned by the {@code RequestWithResult}.
+ */
+class FailureReportForNonVoidRequest<R> {
+
+    private final Supplier<R> retry;
+
+    private final RuntimeException lastException;
+
+    private final ImmutableList<RuntimeException> previousExceptions;
+
+    FailureReportForNonVoidRequest(Supplier<R> retry,
+                                   ImmutableList<RuntimeException> previousExceptions,
+                                   RuntimeException lastException) {
+        this.retry = retry;
+        this.previousExceptions = previousExceptions;
+        this.lastException = lastException;
+    }
+
+    /**
+     * Returns a predefined {@code ActionWithResult} that retries
+     * failed {@code RequestWithResult}.
+     */
+    public ActionWithResult<R> retry() {
+        return retry::get;
+    }
+
+    /**
+     * Returns a predefined {@code ActionWithResult} that propagates current and previously
+     * occurred exceptions as an {@code ExecutionFailedException}.
+     */
+    public ActionWithResult<R> propagate() {
+        ImmutableList<RuntimeException> exceptions = ImmutableList.<RuntimeException>builder()
+                .addAll(previousExceptions)
+                .add(lastException)
+                .build();
+        throw new ExecutionFailedException(exceptions);
+    }
+
+    /**
+     * Returns last occurred exception.
+     */
+    public Exception lastException() {
+        return lastException;
+    }
+
+    /**
+     * Returns a list of all previously occurred exceptions.
+     */
+    public ImmutableList<RuntimeException> previousExceptions() {
+        return previousExceptions;
+    }
+}
