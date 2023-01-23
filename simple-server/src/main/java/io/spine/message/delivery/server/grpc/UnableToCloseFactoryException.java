b8d29a3b5d83eb881438f88014b8dfced54ea6a3
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-01-23 20:02:29 +0100

    Make class final.

--- simple-server/src/main/java/io/spine/message/delivery/server/grpc/UnableToCloseFactoryException.java
+++ simple-server/src/main/java/io/spine/message/delivery/server/grpc/UnableToCloseFactoryException.java
@@ -9,7 +9,7 @@ package io.spine.message.delivery.server.grpc;
 /**
  * There was some problem attempting to close {@code StorageFactory}.
  */
-public class UnableToCloseFactoryException extends RuntimeException {
+public final class UnableToCloseFactoryException extends RuntimeException {
 
     private static final long serialVersionUID = 7916208607916062536L;
 
