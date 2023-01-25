76bc0201079a47e1419575750bbbab52b654689e
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-01-25 14:07:06 +0100

    Call `super()`.

--- server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
+++ server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
@@ -36,6 +36,7 @@ public final class AdminService extends AdminServiceGrpc.AdminServiceImplBase {
      * Creates a new {@code AdminService} with the given {@code client}.
      */
     public AdminService(Supplier<Client> client) {
+        super();
         this.client = checkNotNull(client.get());
     }
 
