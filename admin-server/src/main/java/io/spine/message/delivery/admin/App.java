a30709cddc23ae46135ca64e620903374558364f
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-01-31 13:04:44 +0100

    Create main App class.

---
+++ admin-server/src/main/java/io/spine/message/delivery/admin/App.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+package io.spine.message.delivery.admin;
+
+import io.micronaut.runtime.Micronaut;
+
+/**
+ * Spine Liquor admin server application.
+ */
+final class App {
+
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        Micronaut.run(args);
+    }
+}
