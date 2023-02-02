9acdc6d2e7aa515abf43682d637347671c5c69ec
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-02-02 15:06:23 +0100

    Check string for emptiness also.

--- admin-server/src/main/java/io/spine/message/delivery/admin/security/HttpBasicAuthCredentials.java
+++ admin-server/src/main/java/io/spine/message/delivery/admin/security/HttpBasicAuthCredentials.java
@@ -6,7 +6,7 @@
 
 package io.spine.message.delivery.admin.security;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static io.spine.util.Preconditions2.checkNotEmptyOrBlank;
 
 /**
  * Username and password credentials that is used in {@code HTTP Basic Auth} scheme.
@@ -18,8 +18,8 @@ public final class HttpBasicAuthCredentials {
     private final String password;
 
     public HttpBasicAuthCredentials(String username, String password) {
-        this.username = checkNotNull(username);
-        this.password = checkNotNull(password);
+        this.username = checkNotEmptyOrBlank(username);
+        this.password = checkNotEmptyOrBlank(password);
     }
 
     /**
