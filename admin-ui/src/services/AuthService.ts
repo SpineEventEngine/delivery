4a2c9ba9a5e684e6d99cf07bbc53bf229e9f1ce0
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-02-12 17:33:37 +0100

    Create Auth service.

---
+++ admin-ui/src/services/AuthService.ts
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+import axios from 'axios';
+import { Api } from "src/services/Api";
+
+/**
+ * Performs a user authentication with HTTP Basic Auth strategy.
+ */
+export class AuthService {
+
+  /**
+   * Tries to authenticate a user with the given `login` and `password` and returns `true` if the
+   * attempt is successfully or `false` otherwise.
+   */
+  static tryAuthenticate(login: string, password: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      axios.head(`${Api.ShardInfo}`, AuthService.options(login, password))
+        .then((response) => {
+          if (response.status === 200) {
+            localStorage.login = login;
+            localStorage.password = password;
+            resolve(true);
+          } else {
+            reject(response);
+          }
+        })
+        .catch((error) => {
+          if (error.code === 'ERR_BAD_REQUEST' && error.response.status === 401) {
+            resolve(false);
+          } else {
+            reject(error);
+          }
+        });
+    });
+  }
+
+  /**
+   * Creates auth options for the `axios` request with the given `login` and `password`.
+   * @private
+   */
+  private static options(login: string, password: string):
+    { auth: { username: string, password: string } } {
+    return {
+      auth: {
+        username: login,
+        password,
+      },
+    };
+  }
+
+  /**
+   * Returns user's login from the local storage.
+   * @private
+   */
+  private static login(): string {
+    return localStorage.login;
+  }
+
+  /**
+   * Returns user's password form the local storage.
+   * @private
+   */
+  private static password(): string {
+    return localStorage.password;
+  }
+
+  /**
+   * Creates auth options for the `axios` request with login and password retrieved
+   * from the local storage.
+   */
+  static authOptions():
+    { auth: { username: string, password: string } } {
+    return this.options(AuthService.login(), AuthService.password());
+  }
+
+  /**
+   * Returns `true` if the user is authenticated.
+   *
+   * This method assumes that the user is authenticated is there is a login stored
+   * in a local storage.
+   */
+  static isAuthenticated(): boolean {
+    return !!AuthService.login();
+  }
+}
