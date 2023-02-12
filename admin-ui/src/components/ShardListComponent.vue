8f9e49f202b19f2ca3268eaa168a696d80740f93
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-02-12 17:31:39 +0100

    Create components for displaying shard info.

---
+++ admin-ui/src/components/ShardListComponent.vue
@@ -0,0 +1,34 @@
+<!--
+  - Copyright (c) 2000-2023 TeamDev. All rights reserved.
+  - TeamDev PROPRIETARY and CONFIDENTIAL.
+  - Use is subject to license terms.
+  -->
+
+<template>
+  <q-list>
+    <div v-for="shard in data.shards" :key="shard.index">
+      <shard-info-component :shard="shard"></shard-info-component>
+      <q-separator spaced inset />
+    </div>
+  </q-list>
+</template>
+
+<script lang="ts" setup>
+import { inject, ref } from 'vue';
+import { ShardService } from 'src/services/ShardService';
+import { ShardInfoList } from "components/models";
+import ShardInfoComponent from "components/ShardInfoComponent.vue";
+
+const data = ref({});
+
+const shardService: ShardService = inject(ShardService.name)!;
+
+shardService.shardInfo().then((received) => {
+  data.value = received as ShardInfoList;
+});
+
+</script>
+
+<style scoped>
+
+</style>
