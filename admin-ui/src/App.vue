420d592db785b08985200fc1bad377758c7c33a8
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-02-12 17:28:24 +0100

    Setup quasar project.

---
+++ admin-ui/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view/>
+</template>
+
+<script lang="ts" setup>
+
+import { provide } from 'vue';
+import { ShardService } from 'src/services/ShardService';
+
+provide(ShardService.name, new ShardService());
+
+</script>
