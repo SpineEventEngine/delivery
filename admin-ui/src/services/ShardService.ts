4042fbc57e000654e2f3689a1b1256c93bbf4748
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-02-12 17:33:54 +0100

    Create ShardService.

---
+++ admin-ui/src/services/ShardService.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+import axios from 'axios';
+import { AuthService } from 'src/services/AuthService';
+import { Api } from 'src/services/Api';
+import router from 'src/router/index';
+
+/**
+ * Allows getting shard information.
+ */
+export class ShardService {
+  private shard_info = Api.ShardInfo;
+
+  /**
+   * Requests current shard status from the server.
+   */
+  shardInfo(): Promise<object> {
+    return new Promise<object>((resolve, reject) => {
+      axios.get(`${this.shard_info}`, AuthService.authOptions())
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((e) => {
+          if (e.code === 'ERR_BAD_REQUEST' && e.response.status === 401) {
+            router.push('login');
+          } else {
+            reject(e);
+          }
+        });
+    });
+  }
+}
