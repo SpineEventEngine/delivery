ae4d099591c2807ca847526908165a93412766a8
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-02-13 14:54:27 +0100

    Add logout button.

--- admin-ui/src/layouts/MainLayout.vue
+++ admin-ui/src/layouts/MainLayout.vue
@@ -3,14 +3,29 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>Liquor Admin</q-toolbar-title>
+        <q-btn @click="logout"
+               v-if="isAuth"
+               icon="logout"
+               color="primary"
+               label="Log Out"
+        />
       </q-toolbar>
     </q-header>
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
 <script lang="ts" setup>
+import { AuthService } from "src/services/AuthService";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+const isAuth = AuthService.isAuthenticated;
+
+function logout() {
+  AuthService.logout();
+  router.push("login");
+}
 </script>
