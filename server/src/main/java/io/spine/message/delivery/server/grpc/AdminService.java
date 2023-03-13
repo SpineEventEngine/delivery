4ce9c94d7223271a61215fd0bf318b99d415cbc3
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-03-13 16:08:33 +0100

    Remove unused import.

--- server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
+++ server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
@@ -30,7 +30,6 @@ import io.spine.server.delivery.ShardIndex;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
