20120f6c821afb46b244628a7bee1c80b4f77e3d
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-03-14 16:58:44 +0100

    Generate events with UUID.

--- admin-server/src/main/java/io/spine/message/delivery/admin/AdminController.java
+++ admin-server/src/main/java/io/spine/message/delivery/admin/AdminController.java
@@ -15,6 +15,7 @@ import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.sse.Event;
 import io.micronaut.security.annotation.Secured;
 import io.spine.message.delivery.admin.grpc.AdminServiceGrpc.AdminServiceBlockingStub;
+import io.spine.message.delivery.admin.grpc.ShardInfoUpdate;
 import jakarta.inject.Inject;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
@@ -24,6 +25,7 @@ import java.util.function.Function;
 import static com.google.common.collect.Iterators.transform;
 import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED;
 import static io.spine.json.Json.toCompactJson;
+import static java.util.UUID.randomUUID;
 
 /**
  * Micronaut controller for the {@code /admin} URL path.
@@ -50,9 +52,19 @@ final class AdminController {
     public Publisher<Event<String>> subscribeOnShardUpdates() throws Exception {
         return withGrpcContext((context) -> {
             var updates = adminService.subscribeToShardUpdates(Empty.getDefaultInstance());
-            return Flux.fromIterable(() -> transform(updates, upd -> Event.of(toCompactJson(upd))))
+            return Flux.fromIterable(() -> transform(updates, AdminController::toEvent))
                        .doFinally(signal -> context.close());
         });
+    }
+
+    /**
+     * Creates a new {@code Event} conversing the given {@code update} to compact JSON
+     * and generating a new {@code UUID} for the event.
+     */
+    private static Event<String> toEvent(ShardInfoUpdate update) {
+        String uuid = randomUUID().toString();
+        return Event.of(toCompactJson(update))
+                    .id(uuid);
     }
 
     /**
@@ -62,7 +74,6 @@ final class AdminController {
      * <p>Doesn't close the context after the call execution, the caller must close the context
      * when it will be appropriate according to business logic of the {@code call}.
      */
-
     private static <T> T
     withGrpcContext(Function<Context.CancellableContext, T> call) throws Exception {
         @SuppressWarnings("resource") // Cannot close context before the call we be completed.
