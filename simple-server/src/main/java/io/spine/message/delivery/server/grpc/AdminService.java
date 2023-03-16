2cbfd1c91fd1f3b4414f0532ebeefa051fd1a9e8
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-03-16 08:27:42 +0100

    Fix typo.

--- simple-server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
+++ simple-server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
@@ -68,7 +68,7 @@ public final class AdminService extends AdminServiceGrpc.AdminServiceImplBase
     }
 
     /**
-     * Notifies all existent subscriber about the new {@code ShardInfoChange}.
+     * Notifies all existent subscribers about the new {@code ShardInfoChange}.
      *
      * If an error occurs when trying to notify subscriber it is marked as invalid and removed from
      * the subscribers list.
