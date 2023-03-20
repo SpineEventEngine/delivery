93c7d2158ca72528a178f78deb492e2b7224c64c
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-03-20 13:25:37 +0100

    Use `newConcurrentHashSet()` and explain choice in doc.

--- server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
+++ server/src/main/java/io/spine/message/delivery/server/grpc/AdminService.java
@@ -29,13 +29,13 @@ import io.spine.server.delivery.ShardIndex;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Sets.newConcurrentHashSet;
 import static io.spine.message.delivery.admin.ShardInfoUpdates.messagesCountChangedTo;
 import static io.spine.message.delivery.admin.ShardInfoUpdates.shardPicked;
 import static io.spine.message.delivery.admin.ShardInfoUpdates.shardUnpicked;
@@ -51,8 +51,19 @@ public final class AdminService extends AdminServiceGrpc.AdminServiceImplBase im
 
     /**
      * Subscribers of the {@code AdminService}.
+     *
+     * <p>We use {@code newConcurrentHashSet()} to avoid {@code ConcurrentModificationException}
+     * in cases when we are iterating over the set to notify subscribers and a new subscriber
+     * arrives and the neighbor thread modifies the collection. Also, it's possible that one thread
+     * iterates over the set to notify subscribers and one subscriber is being closed at this moment
+     * and removed from the collection. In the last scenario if we started to iterate over
+     * the collection before the subscriber is closed and removed most probably we will get
+     * the closed subscriber during the iteration and will try to notify it. But this will not lead
+     * to a problem because the {@link #notifySubs(ShardInfoUpdate)} handles invalid subscribers
+     * removing them from the collection, which is not a problem also, even if we will try
+     * to remove an already removed element.
      */
-    private final Set<StreamObserver<ShardInfoUpdate>> subscribers = new HashSet<>();
+    private final Set<StreamObserver<ShardInfoUpdate>> subscribers = newConcurrentHashSet();
 
     /**
      * Maps a {@code ShardIndex} to the number of messages currently available in the shard.
