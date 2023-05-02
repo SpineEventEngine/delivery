a33dbfcdbf6f415093ebed37cf28267669135f9f
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-05-02 14:26:42 +0200

    Add docs.

--- model/src/main/java/io/spine/message/delivery/admin/FilteringObserver.java
+++ model/src/main/java/io/spine/message/delivery/admin/FilteringObserver.java
@@ -18,10 +18,14 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * arrives first. All the updates that may be posted before acknowledging response
  * are omitted by this observer.
  */
-public final class FilteringObserver extends DelegatingServerCallStreamObserver<SubscriptionResponse> {
+public final class FilteringObserver
+        extends DelegatingServerCallStreamObserver<SubscriptionResponse> {
 
     private final AtomicBoolean isAcknowledged = new AtomicBoolean(false);
 
+    /**
+     * Creates a new {@code FilteringObserver} with the given {@code delegate}.
+     */
     public FilteringObserver(ServerCallStreamObserver<SubscriptionResponse> delegate) {
         super(delegate);
     }
