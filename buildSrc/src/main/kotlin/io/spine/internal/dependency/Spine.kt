b63760e7a8576d25cea83df00b3e802150949028
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-05-05 09:20:05 +0200

    Update `spine-base` and `spine-time` in another dependencies file.

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -26,9 +26,9 @@ object Spine {
 
     object Stable {
 
-        const val version = "1.8.0"
+        const val version = "1.9.0-SNAPSHOT.5"
         const val coreVersion = "1.9.0-SNAPSHOT.11"
-        const val timeVersion = "1.8.0"
+        const val timeVersion = "1.9.0-SNAPSHOT.5"
 
         const val base = "io.spine:spine-base:${version}"
         const val client = "io.spine:spine-client:${coreVersion}"
