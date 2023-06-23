accfcce0ca85aff0bcab4f1dbece4c9ed577fed2
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-06-23 10:26:46 +0200

    Create Hazelcast record storage and factory.

---
+++ hazelcast-record-storage/src/main/java/io/spine/server/storage/hazelcast/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+/**
+ * This package provides implementation of Hazelcast-based storages.
+ */
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package io.spine.server.storage.hazelcast;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+
+import javax.annotation.ParametersAreNonnullByDefault;
