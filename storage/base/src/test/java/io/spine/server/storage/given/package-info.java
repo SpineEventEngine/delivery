24496ff2f535b4f34c7ea3d1092f5b32f3447689
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-06-23 12:12:29 +0200

    Extract common parts of storages to the `:storage:base` module.

---
+++ storage/base/src/test/java/io/spine/server/storage/given/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+/**
+ * Test environment classes for the {@code io.spine.server.storage.memory} package.
+ */
+
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package io.spine.server.storage.given;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+
+import javax.annotation.ParametersAreNonnullByDefault;
