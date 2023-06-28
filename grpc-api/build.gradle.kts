195908490ed842abe38dc4ca363941b0dd97e9a5
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-06-28 10:54:35 +0200

    Move admin-related classes to the new module.

---
+++ grpc-api/build.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2000-2023 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+import io.spine.internal.dependency.Grpc
+import io.spine.internal.dependency.Spine
+
+dependencies {
+    Grpc.apply {
+        api(stub)
+        api(protobuf)
+    }
+    implementation(Spine.server)
+}
+
+apply {
+    from(rootDir.toPath().resolve("test-artifacts.gradle"))
+}
