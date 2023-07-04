26656b642b1db7bfe802596fcb79872c30617210
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-07-04 12:52:13 +0200

    Remove copied `.proto` on each `clean` task execution.

--- client/base/build.gradle.kts
+++ client/base/build.gradle.kts
@@ -22,13 +22,30 @@ apply {
     from(rootDir.toPath().resolve("test-artifacts.gradle"))
 }
 
+// Path to the `/proto` directory for this module.
+val localProtoPath = "./src/main/proto"
+
 // We're explicitly copying protos to ensure rejections are generated.
 // See https://github.com/SpineEventEngine/base/issues/650 for details.
 val copyExternalProtos = tasks.create<Copy>("copyExternalProtos") {
     from("../../model/src/main/proto", "../../grpc-api/src/main/proto")
-    into("./src/main/proto")
+    into(localProtoPath)
+}
+
+// We're explicitly deleting copied protos to ensure the build is starting from scratch after
+// each `clean` task.
+//
+// This is useful when external protos are changed (moved or files are renamed) but in this
+// module we still have a copy of not updated files which makes the build pass, masking
+// some possible import issues.
+val deleteCopiedProto = tasks.create<Delete>("deleteCopiedProto") {
+    delete(localProtoPath)
 }
 
 tasks.withType<com.google.protobuf.gradle.GenerateProtoTask> {
     dependsOn(copyExternalProtos)
+}
+
+tasks.clean {
+    dependsOn(deleteCopiedProto)
 }
