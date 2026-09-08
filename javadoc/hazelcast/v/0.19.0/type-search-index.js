1d9cda83347dfa545a89fd72e311a5ae9ca274f4
UpdateGitHubPages Plugin <developers@spine.io>
2026-09-08 13:55:35 +0000

    Update `javadoc` documentation for the module `hazelcast` with the version `0.19.0`.

---
+++ javadoc/hazelcast/v/0.19.0/type-search-index.js
@@ -0,0 +1,1 @@
+var typeSearchIndex = [{"p":"io.spine.server.storage.hazelcast","l":"HazelcastStorageFactory","url":"io/spine/server/storage/hazelcast/HazelcastStorageFactory.html"}, {"p":"io.spine.server.storage.hazelcast","l":"HazelcastRecordStorage","url":"io/spine/server/storage/hazelcast/HazelcastRecordStorage.html"}, {"l":"All classes","url":"allclasses.html"}]
